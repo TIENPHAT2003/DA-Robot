e61b431d8a866fb61c5ccfe417000390c1db3203
TIENPHAT2003 <vutienphat1404@gmail.com>
2024-10-22 02:48:51 +0700

    Add code web

--- Code/InverseKinematic.js
+++
@@ -1,7 +0,0 @@
-function updatePosition(joint) {
-    const inputValue = document.getElementById(`${joint}`).value;
-    const sliderValue = document.getElementById(`${joint}-slider`).value;
-    document.getElementById(`${joint}-value`).textContent = inputValue;
-    document.getElementById(`${joint}-slider`).value = inputValue;
-    document.getElementById(`${joint}`).value = sliderValue;
-}
